25a2fd4a4c42fef09e7ce16a1d97611e851aa099
Leonidas Kalpaxis <leondaki@gmail.com>
2023-01-09 20:46:03 -0500

    Change Test Text

--- Poker Tracker/ContentView.swift
+++ Poker Tracker/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "heart.fill")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                .padding()
+            Text("Poker Tracker Main View")
         }
         .padding()
     }
